6cea9ac7e71c7afe1be768a35bfc90cc4414a7fe
Chema <chema.martinez@wazuh.com>
2017-12-14 07:14:21 -0800

    Cis-cat wodle for Linux Module configuration Integration of CIS-CAT scanner Reports generation

--- src/wazuh_modules/wmodules.h
+++ src/wazuh_modules/wmodules.h
@@ -54,6 +54,7 @@ typedef struct wmodule {
 
 #include "wm_oscap.h"
 #include "wm_database.h"
+#include "wm_ciscat.h"
 
 extern wmodule *wmodules;       // Loaded modules.
 extern int wm_task_nice;        // Nice value for tasks.
